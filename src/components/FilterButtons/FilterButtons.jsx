4b1f34866c9b93553e9243bf4407a363b4990a84
SBodescu <stefan.bodescu@cognizant.com>
2026-02-11 10:52:25 +0200

    feat:search bar implemented and ui for filters

---
+++ src/components/FilterButtons/FilterButtons.jsx
@@ -0,0 +1,10 @@
+import "./FilterButtons.css"
+
+export default function FilterButtons(){
+    return(
+        <div className="filters-container">
+            <button>Genre</button>
+            <button>Rating</button>
+        </div>
+    );
+}
---
+++ src/components/PageButtons/PageButtons.jsx
@@ -0,0 +1,10 @@
+import "./PageButtons.css"
+
+export default function PageButtons(){
+    return(
+        <div className="page-buttons-container">
+            <button className="nav-btn">Home</button>
+            <button className="nav-btn">Watchlist</button>
+        </div>
+    );
+}
